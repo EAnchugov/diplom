31b65e4dccad7a076cc5ad7bf9973f145ef74727
bergen <bergy@yandex.ru>
2023-03-13 00:52:20 +0300

    add smth

--- main-server/src/main/java/ru/practicum/mainserver/controller/AdminUserController.java
+++ main-server/src/main/java/ru/practicum/mainserver/controller/AdminUserController.java
@@ -8,6 +8,9 @@ import ru.practicum.mainserver.model.User.UserMapper;
 import ru.practicum.mainserver.service.user.AdminUserService;
 
 import javax.validation.Valid;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,8 +19,10 @@ public class AdminUserController {
     private final AdminUserService service;
 
     @GetMapping
-    public String getUser() {
-        return null;
+    public List<UserDto> getUser(@RequestParam(required = false) List<Integer> users,
+                          @PositiveOrZero @RequestParam(name = "from", defaultValue = "0") Integer from,
+                          @PositiveOrZero @RequestParam(name = "size", defaultValue = "10") Integer size) {
+        return service.getUsers(users,from,size).stream().map(UserMapper::toUserDto).collect(Collectors.toList());
     }
 
     @PostMapping
--- main-server/src/main/java/ru/practicum/mainserver/repository/UserRepository.java
+++ main-server/src/main/java/ru/practicum/mainserver/repository/UserRepository.java
@@ -3,5 +3,8 @@ package ru.practicum.mainserver.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.mainserver.model.User.User;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User,Integer> {
+    List<User> findAllByIdIn(List<Integer> ids);
 }
