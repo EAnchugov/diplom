9b7b7153b8f7c9cce5d73dfbd6ef52ec8d8f50b2
bergen <bergy@yandex.ru>
2023-03-12 16:10:42 +0300

    try to start main server

---
+++ main-server/src/main/java/ru/practicum/mainserver/controller/TestController.java
@@ -0,0 +1,12 @@
+package ru.practicum.mainserver.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @PostMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
