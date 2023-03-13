bf1b5e1144659684c06717f9ecb1be48343ede89
bergen <bergy@yandex.ru>
2023-03-13 22:51:28 +0300

    add PublicEvents

--- main-server/src/main/java/ru/practicum/mainserver/events/controller/PublicEventsController.java
+++ main-server/src/main/java/ru/practicum/mainserver/events/controller/PublicEventsController.java
@@ -5,18 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.mainserver.events.model.Events;
+import ru.practicum.mainserver.events.model.EventsFullDto;
+import ru.practicum.mainserver.events.model.EventsMapper;
+import ru.practicum.mainserver.events.service.EventsService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/events")
 @RequiredArgsConstructor
 public class PublicEventsController {
+    private final EventsService service;
     @GetMapping
-    public String getEvents() {
-        return null;
-    }
+    public List<EventsFullDto> getEvents() {
+        return service.getAll().stream().map(EventsMapper::toEventsFullDto)
+                .collect(Collectors.toList());
+    };
 
     @GetMapping("/{id}")
-    public String getEventById(@PathVariable Integer id) {
-        return null;
+    public EventsFullDto getEventById(@PathVariable Integer id) {
+        return EventsMapper.toEventsFullDto(service.getById(id));
     }
 }
