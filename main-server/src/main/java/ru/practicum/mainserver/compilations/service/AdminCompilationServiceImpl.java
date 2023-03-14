ff254bf89b58ff1e37e027b1d6e63c2fccc74d2c
bergen <bergy@yandex.ru>
2023-03-14 17:37:06 +0300

    minor fix

--- main-server/src/main/java/ru/practicum/mainserver/compilations/service/AdminCompilationServiceImpl.java
+++ main-server/src/main/java/ru/practicum/mainserver/compilations/service/AdminCompilationServiceImpl.java
@@ -1,7 +1,9 @@
 package ru.practicum.mainserver.compilations.service;
 
+import org.springframework.stereotype.Service;
 import ru.practicum.mainserver.compilations.model.Compilation;
 
+@Service
 public class AdminCompilationServiceImpl implements AdminCompilationService {
     @Override
     public Compilation create(Compilation compilation) {
