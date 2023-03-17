afbb1b047daf27e5892ad6fffaa07bb935f5ff1d
bergen <bergy@yandex.ru>
2023-03-18 00:00:09 +0300

    add getUserEvents fix

--- ewm-service/src/main/java/ru/practicum/categories/service/user/UserCategoryServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/categories/service/user/UserCategoryServiceImpl.java
@@ -3,15 +3,12 @@ package ru.practicum.categories.service.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import ru.practicum.categories.model.Category;
 import ru.practicum.categories.repository.CategoryRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @Service
 @RequiredArgsConstructor
@@ -20,7 +17,7 @@ public class UserCategoryServiceImpl implements UserCategoryService {
 
     @Override
     public List<Category> getAll(Integer from, Integer size) {
-        Pageable pageable = PageRequest.of(from > 0 ? from / size : 0, size, Sort.by(DESC, "start"));
+        Pageable pageable = PageRequest.of(from,size);
         return repository.findAll(pageable).stream().collect(Collectors.toList());
     }
 
