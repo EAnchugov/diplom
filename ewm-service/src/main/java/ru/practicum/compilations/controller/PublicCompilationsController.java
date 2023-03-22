8a68d1f9012966cd5349a68832bb303c491b2731
bergen <bergy@yandex.ru>
2023-03-22 16:56:27 +0300

    add compilations getAll fix

--- ewm-service/src/main/java/ru/practicum/compilations/controller/PublicCompilationsController.java
+++ ewm-service/src/main/java/ru/practicum/compilations/controller/PublicCompilationsController.java
@@ -1,15 +1,26 @@
 package ru.practicum.compilations.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.compilations.model.CompilationDto;
+import ru.practicum.compilations.model.CompilationMapper;
+import ru.practicum.compilations.service.CompilationService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/compilations")
+@AllArgsConstructor
 public class PublicCompilationsController {
+    private final CompilationService compilationService;
+
     @GetMapping
-    public String getAllCompilations(@RequestParam Boolean pinned,
-                                  @RequestParam Integer from,
-                                  @RequestParam Integer size) {
-        return null;
+    public List<CompilationDto> getAllCompilations(@RequestParam Boolean pinned,
+                                                   @RequestParam Integer from,
+                                                   @RequestParam Integer size) {
+        return compilationService.getAll(pinned,from,size).stream().map(CompilationMapper::toCompilationDto)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/{complId}")
--- ewm-service/src/main/java/ru/practicum/compilations/service/CompilationService.java
+++ ewm-service/src/main/java/ru/practicum/compilations/service/CompilationService.java
@@ -2,6 +2,10 @@ package ru.practicum.compilations.service;
 
 import ru.practicum.compilations.model.Compilation;
 
-public interface AdminCompilationService {
+import java.util.List;
+
+public interface CompilationService {
     Compilation create(Compilation compilation);
+
+    List<Compilation> getAll(Boolean pinned, Integer from, Integer size);
 }
