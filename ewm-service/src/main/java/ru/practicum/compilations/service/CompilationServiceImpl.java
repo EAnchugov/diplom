8c359212551a63f0e7fdba185370aafbdba2285f
bergen <bergy@yandex.ru>
2023-03-22 18:23:29 +0300

    add compilations fix

--- ewm-service/src/main/java/ru/practicum/compilations/service/CompilationServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/compilations/service/CompilationServiceImpl.java
@@ -7,6 +7,7 @@ import ru.practicum.compilations.model.Compilation;
 import ru.practicum.compilations.repository.CompilationRepository;
 import org.springframework.data.domain.Pageable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,11 +22,13 @@ public class CompilationServiceImpl implements CompilationService {
 
     @Override
     public List<Compilation> getAll(Boolean pinned, Integer from, Integer size) {
+        List<Compilation> compilations = new ArrayList<>();
         Pageable pageable = PageRequest.of(from,size);
-        if (pinned == null) {
-            return repository.findAll(pageable).toList();
+        if (!pinned) {
+            compilations.addAll(repository.findAll(pageable).toList());
         } else {
-            return repository.findAllByPinned(pinned,pageable);
+            compilations.addAll(repository.findAllByPinned(pinned,pageable));
         }
+        return compilations;
     }
 }
