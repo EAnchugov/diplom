ede8e02ebe5f3a5ec40733ac0afe7c61a0512ed5
bergen <bergy@yandex.ru>
2023-03-23 22:38:37 +0300

    add findById fix

---
+++ ewm-service/src/main/java/ru/practicum/exceptions/ErrorHandler.java
@@ -0,0 +1,19 @@
+package ru.practicum.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private ErrorResponse handle(final ItemNotAvailableException e) {
+        log.error("Ошибка ItemNotAvailableException " + e.getMessage());
+        return new ErrorResponse(
+                e.getMessage());
+    }
+}
