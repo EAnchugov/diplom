a30f81cdc0d298d95ee79c5f8bd255cb5996a34d
bergen <bergy@yandex.ru>
2023-03-23 17:34:54 +0300

    add patchCategory fix

--- ewm-service/src/main/java/ru/practicum/categories/service/admin/AdminCategoriesService.java
+++ ewm-service/src/main/java/ru/practicum/categories/service/admin/AdminCategoriesService.java
@@ -1,12 +1,11 @@
 package ru.practicum.categories.service.admin;
 
 import ru.practicum.categories.model.Category;
-import ru.practicum.categories.model.CategoryDto;
 
 public interface AdminCategoriesService {
     Category createCategory(Category category);
 
     void deleteCategoryById(Integer catId);
 
-    Category patchCategory(Integer catId, CategoryDto categoryDto);
+    Category patchCategory(Integer catId);
 }
