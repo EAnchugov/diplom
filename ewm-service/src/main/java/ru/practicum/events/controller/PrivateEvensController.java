cd449c0c9bb3f2109da345f8bf2ed0bd341cebb3
bergen <bergy@yandex.ru>
2023-03-28 19:03:47 +0300

    UpdateEventUserRequest fix

--- ewm-service/src/main/java/ru/practicum/events/controller/PrivateEvensController.java
+++ ewm-service/src/main/java/ru/practicum/events/controller/PrivateEvensController.java
@@ -37,10 +37,10 @@ public class PrivateEvensController {
     }
 
     @PatchMapping("/{userId}/events/{eventId}")
-    public Event patchUserEvent(@PathVariable @Positive Integer userId,
+    public EventsFullDto patchUserEvent(@PathVariable @Positive Integer userId,
                                 @PathVariable @Positive Integer eventId,
                                 @RequestBody @Validated UpdateEventUserRequest updateEventUserRequest) {
-        return service.updateEvent(userId, eventId, updateEventUserRequest);
+        return EventsMapper.toEventsFullDto(service.updateEvent(userId, eventId, updateEventUserRequest));
     }
 
     @GetMapping("/{userId}/events/{eventId}/requests")
