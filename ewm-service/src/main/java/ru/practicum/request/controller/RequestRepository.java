084abe5d803f51377b61e1e2105eefa444ad4054
bergen <bergy@yandex.ru>
2023-03-31 22:47:34 +0300

    add exception in create request

--- ewm-service/src/main/java/ru/practicum/request/controller/RequestRepository.java
+++ ewm-service/src/main/java/ru/practicum/request/controller/RequestRepository.java
@@ -1,7 +1,12 @@
 package ru.practicum.request.controller;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.practicum.events.model.Event;
 import ru.practicum.request.model.Request;
+import ru.practicum.user.model.User;
+
+import java.util.Optional;
 
 public interface RequestRepository extends JpaRepository<Request, Integer> {
+    Optional<Request> findAllByRequesterAndEvent(User requester, Event event);
 }
