d32a562ccd1c6e33299e3ce4b0541e4d29077d0e
bergen <bergy@yandex.ru>
2023-04-01 01:22:53 +0300

    add approveRequest

--- ewm-service/src/main/java/ru/practicum/request/service/RequestService.java
+++ ewm-service/src/main/java/ru/practicum/request/service/RequestService.java
@@ -1,9 +1,14 @@
 package ru.practicum.request.service;
 
+import ru.practicum.events.model.Event;
 import ru.practicum.request.model.Request;
+
+import java.util.List;
 
 public interface RequestService {
     Request create(Integer userId, Integer eventId);
 
     void approveRequest(Integer userId, Integer eventId);
+
+    List<Request> getAllByEvent(Event event);
 }
