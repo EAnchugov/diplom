ed7302bddc87e7605210c52e760a71c602c43fde
bergen <bergy@yandex.ru>
2023-04-01 13:31:52 +0300

    EventRequestStatusUpdateRequest fix

--- ewm-service/src/main/java/ru/practicum/events/model/EventRequestStatusUpdateRequest.java
+++ ewm-service/src/main/java/ru/practicum/events/model/EventRequestStatusUpdateRequest.java
@@ -1,10 +1,22 @@
 package ru.practicum.events.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.practicum.request.model.Status;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class EventRequestStatusUpdateRequest {
+    @NotEmpty
     List<Integer> ids;
+    @NotBlank
     Status status;
 }
