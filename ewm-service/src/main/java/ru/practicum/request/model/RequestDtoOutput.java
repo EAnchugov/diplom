d9c3636fe498d95bcd7359c2ff8b7c968473987e
bergen <bergy@yandex.ru>
2023-04-01 19:08:24 +0300

    EventRequestStatusUpdateRequest fix

--- ewm-service/src/main/java/ru/practicum/request/model/RequestDtoOutput.java
+++ ewm-service/src/main/java/ru/practicum/request/model/RequestDtoOutput.java
@@ -1,14 +1,14 @@
 package ru.practicum.request.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestDtoOutput {
     private LocalDateTime created;
     private Integer event;
