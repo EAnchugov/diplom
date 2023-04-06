b63171d3a35912c2abfd8327b18c6654de2b5df3
bergen <bergy@yandex.ru>
2023-04-06 23:03:23 +0300

    test

--- ewm-service/src/main/java/ru/practicum/events/repository/EventsRepository.java
+++ ewm-service/src/main/java/ru/practicum/events/repository/EventsRepository.java
@@ -27,6 +27,12 @@ public interface EventsRepository extends JpaRepository<Event,Integer> {
     List<Event> testMethod(Integer users, State state, Integer categories, LocalDateTime start, LocalDateTime end, Pageable pageable);
 
     @Query("SELECT e from Event e " +
+            "where e.initiator.id = :users and " +
+            "e.category.id = :categories and " +
+            "e.publishedOn between :start and :end")
+    List<Event> testMethod2(Integer users, Integer categories, LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+    @Query("SELECT e from Event e " +
             "where e.category.id = :category and " +
             "e.paid = :paid and " +
             "e.publishedOn between :start and :end and " +
