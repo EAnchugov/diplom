0cc10443467953beaa21238f315ab4b9a7d71c06
bergen <bergy@yandex.ru>
2023-04-07 02:02:46 +0300

    test add compilation delete

--- ewm-service/src/main/java/ru/practicum/events/controller/AdminEventsController.java
+++ ewm-service/src/main/java/ru/practicum/events/controller/AdminEventsController.java
@@ -3,6 +3,7 @@ package ru.practicum.events.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.events.model.Event;
 import ru.practicum.events.model.EventDtoOutput;
 import ru.practicum.events.model.EventsMapper;
 import ru.practicum.events.model.UpdateEventUserRequest;
@@ -11,7 +12,6 @@ import ru.practicum.events.service.EventsService;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/admin/events")
@@ -21,16 +21,17 @@ public class AdminEventsController {
     private final EventsService eventsService;
 
     @GetMapping
-    public List<EventDtoOutput> getFilteredEvents(@RequestParam(required = false) Integer users,
-                                 @RequestParam(required = false) String states,
-                                 @RequestParam(required = false) Integer categories,
-                                 @RequestParam(required = false) String rangeStart,
-                                 @RequestParam(required = false) String rangeEnd,
-                                 @Positive @RequestParam(defaultValue = "0") Integer from,
-                                 @PositiveOrZero @RequestParam(defaultValue = "10") Integer size
+    public List<Event> getFilteredEvents(@RequestParam(required = false) Integer users,
+                                         @RequestParam(required = false) String states,
+                                         @RequestParam(required = false) Integer categories,
+                                         @RequestParam(required = false) String rangeStart,
+                                         @RequestParam(required = false) String rangeEnd,
+                                         @Positive @RequestParam(defaultValue = "0") Integer from,
+                                         @PositiveOrZero @RequestParam(defaultValue = "10") Integer size
     ) {
-        return eventsService.getFilteredEvents(users,states,categories,rangeStart,rangeEnd,from,size).stream()
-                .map(EventsMapper::eventToOutput).collect(Collectors.toList());
+        return eventsService.getFilteredEvents(users,states,categories,rangeStart,rangeEnd,from,size);
+//        .stream()
+//                .map(EventsMapper::eventToOutput).collect(Collectors.toList());
     }
 
     @PatchMapping("/{eventId}")
