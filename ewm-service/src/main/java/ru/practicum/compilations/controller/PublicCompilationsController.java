980264fe88efe02eb7f2334b114aa3fd23c82d07
EAnchugov <bergy@yandex.ru>
2023-04-07 21:15:15 +0300

    Compilation test

--- ewm-service/src/main/java/ru/practicum/compilations/controller/PublicCompilationsController.java
+++ ewm-service/src/main/java/ru/practicum/compilations/controller/PublicCompilationsController.java
@@ -1,32 +1,32 @@
-//package ru.practicum.compilations.controller;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.*;
-//import ru.practicum.compilations.model.CompilationDto;
-//import ru.practicum.compilations.service.CompilationService;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/compilations")
-//@AllArgsConstructor
-//@Slf4j
-//public class PublicCompilationsController {
-//    private final CompilationService compilationService;
-//
-//    @GetMapping
-//    public List<CompilationDto> getAllCompilations(@RequestParam(defaultValue = "false") Boolean pinned,
-//                                                   @RequestParam(defaultValue = "0") Integer from,
-//                                                   @RequestParam(defaultValue = "10") Integer size) {
-////        log.info("Получен запрос getAllCompilations pinned={}, from={}, size={}", pinned, from, size);
-////        return compilationService.getAll(pinned,from,size).stream().map(CompilationMapper::toCompilationDto)
-////                        .collect(Collectors.toList());
-//        return null;
-//    }
-//
-//    @GetMapping("/{compId}")
-//    public String getCompilations(@PathVariable Integer compId) {
-//        return null;
-//    }
-//}
+package ru.practicum.compilations.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.compilations.model.CompilationDto;
+import ru.practicum.compilations.service.CompilationService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/compilations")
+@AllArgsConstructor
+@Slf4j
+public class PublicCompilationsController {
+    private final CompilationService compilationService;
+
+    @GetMapping
+    public List<CompilationDto> getAllCompilations(@RequestParam(defaultValue = "false") Boolean pinned,
+                                                   @RequestParam(defaultValue = "0") Integer from,
+                                                   @RequestParam(defaultValue = "10") Integer size) {
+//        log.info("Получен запрос getAllCompilations pinned={}, from={}, size={}", pinned, from, size);
+//        return compilationService.getAll(pinned,from,size).stream().map(CompilationMapper::toCompilationDto)
+//                        .collect(Collectors.toList());
+        return null;
+    }
+
+    @GetMapping("/{compId}")
+    public String getCompilations(@PathVariable Integer compId) {
+        return null;
+    }
+}
