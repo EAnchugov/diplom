a1073448fa31cf694a3a0ec25533ae70d789d22c
EAnchugov <bergy@yandex.ru>
2023-04-07 22:07:47 +0300

    getAllCompilations test

--- ewm-service/src/main/java/ru/practicum/compilations/controller/AdminCompilationsController.java
+++ ewm-service/src/main/java/ru/practicum/compilations/controller/AdminCompilationsController.java
@@ -1,8 +1,13 @@
 package ru.practicum.compilations.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.compilations.model.Compilation;
+import ru.practicum.compilations.model.CompilationDtoInput;
 import ru.practicum.compilations.service.CompilationService;
 
 @RequiredArgsConstructor
@@ -11,16 +16,16 @@ import ru.practicum.compilations.service.CompilationService;
 public class AdminCompilationsController {
     private final CompilationService service;
 
-//    @PostMapping
-//    public Compilation addCompilation(@RequestBody @Validated CompilationDtoInput input) {
-////        Compilation compilation = service.create(Compilation.builder()
-////                        .title(input.getTitle())
-////                        .pinned(input.getPinned())
-////                        .build());
-////
-////        return compilation;
-//        return new Compilation();
-//    }
+    @PostMapping
+    public Compilation addCompilation(@RequestBody @Validated CompilationDtoInput input) {
+//        Compilation compilation = service.create(Compilation.builder()
+//                        .title(input.getTitle())
+//                        .pinned(input.getPinned())
+//                        .build());
+//
+//        return compilation;
+        return new Compilation();
+    }
 
 //    @DeleteMapping("/{compId}")
 //    public void deleteCompilation(@PathVariable Integer compId) {
