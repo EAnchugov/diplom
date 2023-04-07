c1c77562f65e7619d124637f8e1370eb76a397bb
EAnchugov <bergy@yandex.ru>
2023-04-08 01:14:08 +0300

    getUserRequests test

--- ewm-service/src/main/java/ru/practicum/request/model/RequestMapper.java
+++ ewm-service/src/main/java/ru/practicum/request/model/RequestMapper.java
@@ -1,15 +1,13 @@
 package ru.practicum.request.model;
 
-import ru.practicum.events.model.EventsMapper;
-import ru.practicum.user.model.UserMapper;
 import ru.practicum.variables.GlobalVariables;
 
 public class RequestMapper {
     public static RequestDtoOutput toOutput(Request request) {
         return RequestDtoOutput.builder()
                 .id(request.getId())
-                .requester(UserMapper.toUserDto(request.getRequester()))
-                .event(EventsMapper.eventToOutput(request.getEvent()))
+                .requester(request.getRequester().getId())
+                .event(request.getEvent().getId())
                 .status(request.getStatus())
                 .created(request.getCreated().format(GlobalVariables.FORMAT)).build();
     }
