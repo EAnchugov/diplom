b5e76e6bbd514e6843f4dd2c329b9d1e1c8d5e8b
EAnchugov <bergy@yandex.ru>
2023-04-08 21:51:24 +0300

    createCompilation test 2

--- ewm-service/src/main/java/ru/practicum/compilations/controller/PublicCompilationsController.java
+++ ewm-service/src/main/java/ru/practicum/compilations/controller/PublicCompilationsController.java
@@ -2,13 +2,9 @@ package ru.practicum.compilations.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-import ru.practicum.compilations.model.CompilationDto;
-import ru.practicum.compilations.model.CompilationMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.compilations.service.CompilationService;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/compilations")
@@ -17,17 +13,17 @@ import java.util.stream.Collectors;
 public class PublicCompilationsController {
     private final CompilationService compilationService;
 
-    @GetMapping
-    public List<CompilationDto> getAllCompilations(@RequestParam(defaultValue = "false") Boolean pinned,
-                                                   @RequestParam(defaultValue = "0") Integer from,
-                                                   @RequestParam(defaultValue = "10") Integer size) {
-//        log.info("Получен запрос getAllCompilations pinned={}, from={}, size={}", pinned, from, size);
-        return compilationService.getAll(pinned,from,size).stream().map(CompilationMapper::toCompilationDto)
-                        .collect(Collectors.toList());
-    }
-
-    @GetMapping("/{compId}")
-    public String getCompilations(@PathVariable Integer compId) {
-        return null;
-    }
+//    @GetMapping
+//    public List<CompilationDto> getAllCompilations(@RequestParam(defaultValue = "false") Boolean pinned,
+//                                                   @RequestParam(defaultValue = "0") Integer from,
+//                                                   @RequestParam(defaultValue = "10") Integer size) {
+////        log.info("Получен запрос getAllCompilations pinned={}, from={}, size={}", pinned, from, size);
+//        return compilationService.getAll(pinned,from,size).stream().map(CompilationMapper::toCompilationDto)
+//                        .collect(Collectors.toList());
+//    }
+//
+//    @GetMapping("/{compId}")
+//    public String getCompilations(@PathVariable Integer compId) {
+//        return null;
+//    }
 }
